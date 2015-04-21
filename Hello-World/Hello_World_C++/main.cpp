f2ac37ec52be403347c12f251f319be8e46fc0e3
Esuabom David Dijemeni <e.dijemeni@imperial.ac.uk>
2015-04-21 13:52:03 +0100

    Hello world in C++

---
+++ Hello-World/Hello_World_C++/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "This is great to know many programming languages" << endl;
+
+    return 0;
+}
